c78f49b684f7fe8bb795d175dfcb813ea3c21443
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-18 18:12:01 +0000

    made page interactive with js

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
+let headerHeight = 77.96;
+
+navAnchors.forEach((anchor) => {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (window.innerWidth > 720) {
+      if (anchor.dataset.id === "home") {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else if (anchor.dataset.id === "about") {
+        window.scrollTo({
+          top: window.innerHeight - headerHeight,
+          behavior: "smooth",
+        });
+      } else if (anchor.dataset.id === "services") {
+        window.scrollTo({
+          top: window.innerHeight * 2 - headerHeight * 2,
+          behavior: "smooth",
+        });
+      } else {
+        window.scrollTo({
+          top: window.innerHeight * 3 - headerHeight * 3,
+          behavior: "smooth",
+        });
+      }
+    } else {
+      if (anchor.dataset.id === "home") {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else if (anchor.dataset.id === "about") {
+        window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+      } else if (anchor.dataset.id === "services") {
+        window.scrollTo({ top: window.innerHeight * 2, behavior: "smooth" });
+      } else {
+        window.scrollTo({ top: window.innerHeight * 3, behavior: "smooth" });
+      }
+    }
+  });
+});
